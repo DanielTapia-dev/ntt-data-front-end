e80a16f743aa44de67d642e11d8685973b3fa57c
DanielTapia-dev <d.tapialopez1@gmail.com>
2025-04-16 03:05:04 -0500

    chore: initial project setup with modular Angular architecture

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AlertComponent } from './shared/components/alert/alert.component';
+import { ConfirmModalComponent } from './shared/components/confirm-modal/confirm-modal.component';
+import { SearchBarComponent } from './shared/components/search-bar/search-bar.component';
+import { TableComponent } from './shared/components/table/table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AlertComponent,
+    ConfirmModalComponent,
+    SearchBarComponent,
+    TableComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/modules/products/products.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ProductsRoutingModule } from './products-routing.module';
+import { ProductsComponent } from './products.component';
+import { ProductFormComponent } from './product-form/product-form.component';
+
+@NgModule({
+  declarations: [ProductsComponent, ProductFormComponent],
+  imports: [CommonModule, ProductsRoutingModule],
+})
+export class ProductsModule {}
